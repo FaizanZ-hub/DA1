dac2a0638ec9726f07245f8a53cb0d7921cdbb7a
Faizan Zafar <zafarfaizan92@gmail.com>
2024-09-09 09:37:26 +0530

    turtle

---
+++ Turtle/Decagon.py
@@ -0,0 +1,8 @@
+from turtle import *
+
+s=getscreen()
+t=Turtle()
+for i in range(10):
+    t.forward(100)
+    t.left(36)
+mainloop()    #using loop
---
+++ Turtle/square.py
@@ -0,0 +1,14 @@
+from turtle import *
+speed("slow")
+
+bgcolor("black")
+
+forward(100)
+left(90)
+forward(100)
+left(90)
+forward(100)
+left(90)
+forward(100)
+
+mainloop() #use to stop the screen
